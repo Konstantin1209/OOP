a508c7d0adacfa4c708ec37b0b2c0dc94a448661
Konstantin1209 <156205730+Konstantin1209@users.noreply.github.com>
2024-03-03 22:22:17 +0500

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -3,16 +3,16 @@ import requests
 from datetime import datetime
 import json
 import os
-TOKEN = str(input('Введите токен ВК: '))
-TOKEN_yan = str(input('Введите токен Яндекс диска: '))
+
+
 class VKAPICllient:
     API_BASE_URL = 'https://api.vk.com/method'
 
-    def __init__(self, token, user_id, size):
+
+    def __init__(self, token, user_id):
         self.token = token
         self.user_id = user_id
-        self.size = size
-       
+      
     def _build_url(self, api_method):
         return f"{self.API_BASE_URL}/{api_method}"
 
@@ -28,11 +28,11 @@ class VKAPICllient:
         response = requests.get(self._build_url('photos.get'), params={**params, **photos_params})
         return response.json()
        
-    def download_dis(self):
+    def download_dis(self, size):
         """скачать в текущую папку на диске и создание json файла"""
         profile_photos = self.get_profile_photos()
         for photos in profile_photos['response']['items']:
-            maximum_size = self.size
+            maximum_size = size
             filtered_list = list(filter(lambda d: maximum_size in d.values(), photos['sizes']))
             for max_size in filtered_list:
                 post_date = datetime.fromtimestamp(photos['date'])
@@ -60,9 +60,9 @@ class VKAPICllient:
 class YandexDisk:
     API_BASE_YANDEX = 'https://cloud-api.yandex.net'
 
-    def __init__(self, token, new_folder):
+
+    def __init__(self, token):
         self.token = token
-        self.new_folder = new_folder
 
     def _build_yandex(self, api_method):
         """Функция для составления запросов"""
@@ -74,19 +74,19 @@ class YandexDisk:
         headers = {'Authorization': author}
         return headers
 
-    def create_dir(self):
+    def create_dir(self, new_folder):
         """создание папки на яндекс диске"""
-        params = {'path': self.new_folder}
+        params = {'path': new_folder}
         response = requests.put(self._build_yandex('v1/disk/resources'), params=params, headers=self._headers())
         return response.json()
     
-    def download_yandex(self):
+    def download_yandex(self, new_folder):
         url_for_upload = self._build_yandex('v1/disk/resources/upload')       
         with open('photo_data.json', 'r') as f:
             json_string = f.read()
         data = json.loads(json_string)
         for d in data:
-            params = {'path': f"{self.new_folder}/{d['name']}"}
+            params = {'path': f"{new_folder}/{d['name']}"}
             response = requests.get(url_for_upload, params=params, headers = self._headers())
             upload_ = response.json().get('href')
             name = d['name']
@@ -95,12 +95,14 @@ class YandexDisk:
 
 
 if __name__ ==  '__main__':
+    TOKEN = str(input('Введите токен ВК: '))
+    TOKEN_yan = str(input('Введите токен Яндекс диска: '))
     user_id = input('введите id пользователя: ')
     size = str(input('введите желаемый размер фото: s, m, x, y, z, 0: '))
     n_folder = input('Введите название папки для яндекс диска: ')
-    yan_disk = YandexDisk(TOKEN_yan, n_folder)
-    vk_client = VKAPICllient(TOKEN, user_id, size)
-    folder = yan_disk.create_dir()
-    pprint(vk_client.download_dis())
-    pprint(yan_disk.download_yandex())
+    yan_disk = YandexDisk(TOKEN_yan)
+    vk_client = VKAPICllient(TOKEN, user_id)
+    folder = yan_disk.create_dir(n_folder)
+    pprint(vk_client.download_dis(size))
+    pprint(yan_disk.download_yandex(n_folder))
 
